502f9671b2f81fc8829a9a7f30a33a3985a9d063
bjr2021 <bjr2022@163.com>
2022-11-02 14:09:20 +0800

    采购退货列表页和详情页

---
+++ src/views/warehouse/purchase/return/detail.vue
@@ -0,0 +1,365 @@
+<template>
+  <div style="height: 100%">
+    <LayoutHeader style="background-color: #fff; height: 60px; padding: 0 20px">
+      <a style="font-size: 1.4rem; font-weight: bold; color: #000; line-height: 60px" @click="back"
+        ><RollbackOutlined /> 返回</a
+      >
+      <div style="display: flex; float: right">
+        <Button type="primary" class="button" @click="onSubmit" v-if="formState.bsStatus !== 'B'"
+          >保存</Button
+        >
+        <Button danger class="button" @click="onExam" v-if="formState.bsStatus === 'A'"
+          >审核</Button
+        >
+        <Button danger class="button" @click="unExam" v-if="formState.bsStatus === 'B'"
+          >反审核</Button
+        >
+      </div>
+    </LayoutHeader>
+    <div class="content">
+      <Tabs v-model:activeKey="activeKey" class="tabs">
+        <TabPane key="1" tab="基本信息">
+          <a-form ref="formRef" :model="formState" :rules="formRules">
+            <Row>
+              <Col :span="8">
+                <a-form-item label="仓库编码：" ref="number" name="number" class="item">
+                  <Input
+                    allowClear
+                    class="input"
+                    autocomplete="off"
+                    v-model:value="formState.number"
+                    :placeholder="formState.bsStatus === 'B' ? '' : '请输入仓库编码'"
+                    :disabled="formState.bsStatus === 'B'"
+                  />
+                </a-form-item>
+              </Col>
+              <Col :span="8">
+                <a-form-item label="仓库名称：" ref="name" name="name" class="item">
+                  <Input
+                    allowClear
+                    class="input"
+                    autocomplete="off"
+                    v-model:value="formState.name"
+                    name="name"
+                    :placeholder="formState.bsStatus === 'B' ? '' : '请输入仓库名称'"
+                    :disabled="formState.bsStatus === 'B'"
+                  />
+                </a-form-item>
+              </Col>
+              <Col :span="8">
+                <a-form-item label="责任人：" ref="empId" name="empId" class="item">
+                  <ExInput
+                    autocomplete="off"
+                    class="input"
+                    :placeholder="formState.bsStatus === 'B' ? '' : '请选择责任人'"
+                    label="责任人"
+                    :show="formState.bsStatus !== 'B'"
+                    v-model:value="formState.bdEmployee"
+                    :disabled="formState.bsStatus === 'B'"
+                    @search="
+                      onSearch('GET_EMPLOYEE_DTO', 'bdEmployee', Url.EMPLOYEE_GET_DATA, [
+                        'empId',
+                        'bdEmployee',
+                      ])
+                    "
+                    @clear="onClear(['empId', 'bdEmployee'])"
+                  />
+                </a-form-item>
+              </Col>
+            </Row>
+            <Row>
+              <Col :span="8">
+                <a-form-item label="联系电话：" ref="phone" name="phone" class="item">
+                  <Input
+                    allowClear
+                    class="input"
+                    autocomplete="off"
+                    v-model:value="formState.phone"
+                    :placeholder="formState.bsStatus === 'B' ? '' : '请输入联系电话'"
+                    :disabled="formState.bsStatus === 'B'"
+                    onkeyup="value=value.replace(/[^\d\-]/g,'')"
+                    :maxlength="20"
+                  />
+                </a-form-item>
+              </Col>
+              <Col :span="8">
+                <a-form-item label="仓库地址：" ref="address" name="address" class="item">
+                  <Input
+                    allowClear
+                    class="input"
+                    autocomplete="off"
+                    v-model:value="formState.address"
+                    name="name"
+                    :placeholder="formState.bsStatus === 'B' ? '' : '请输入仓库地址'"
+                    :disabled="formState.bsStatus === 'B'"
+                  />
+                </a-form-item>
+              </Col>
+              <Col :span="8">
+                <a-form-item label="业务状态：" ref="bsStatus" name="bsStatus" class="item">
+                  <Input
+                    allowClear
+                    class="input"
+                    :value="config.BS_STATUS[formState.bsStatus] || '暂存'"
+                    name="bsStatus"
+                    :disabled="true"
+                  />
+                </a-form-item>
+              </Col>
+            </Row>
+            <Row>
+              <Col :span="8">
+                <a-form-item label="备注：" ref="mark" name="mark" class="item">
+                  <a-textArea
+                    v-model:value="formState.mark"
+                    :placeholder="formState.bsStatus === 'B' ? '' : '请添加备注'"
+                    :rows="3"
+                    class="textArea"
+                    :disabled="formState.bsStatus === 'B'"
+                  />
+                </a-form-item>
+              </Col>
+            </Row>
+          </a-form>
+        </TabPane>
+        <TabPane key="2" tab="其他信息">
+          <a-form ref="formRef" :model="formState" :rules="formRules">
+            <Row>
+              <Col :span="8">
+                <a-form-item label="创建时间：" ref="createTime" name="createTime" class="item">
+                  <Input class="input" v-model:value="formState.createTime" disabled />
+                </a-form-item>
+              </Col>
+              <Col :span="8">
+                <a-form-item label="创建人：" ref="createBy" name="createBy" class="item">
+                  <Input class="input" v-model:value="formState.createBy" disabled />
+                </a-form-item>
+              </Col>
+            </Row>
+            <Row>
+              <Col :span="8">
+                <a-form-item label="修改时间：" ref="updateTime" name="updateTime" class="item">
+                  <Input
+                    class="input"
+                    v-model:value="formState.updateTime"
+                    disabled
+                  /> </a-form-item
+              ></Col>
+              <Col :span="8">
+                <a-form-item label="修改人：" ref="updateBy" name="updateBy" class="item">
+                  <Input class="input" v-model:value="formState.updateBy" disabled /> </a-form-item
+              ></Col>
+            </Row>
+          </a-form>
+        </TabPane>
+      </Tabs>
+    </div>
+    <BasicSearch
+      @basicClickEvent="basicClickEvent"
+      :gridOptions="basicGridOptions"
+      ref="basicSearchRef"
+      :control="basicControl"
+      :tableCols="basicTableCols"
+      :tableName="basicTableName"
+    />
+  </div>
+</template>
+<script lang="ts" setup>
+  import { onMounted, reactive, ref, toRef } from 'vue';
+  import {
+    Button,
+    Col,
+    Form,
+    FormItem,
+    Input,
+    LayoutHeader,
+    Row,
+    TabPane,
+    Tabs,
+  } from 'ant-design-vue';
+  import { BasicSearch } from '/@/components/AMoreSearch';
+  import { RollbackOutlined } from '@ant-design/icons-vue';
+  import { useRoute, useRouter } from 'vue-router';
+  import { VXETable } from 'vxe-table';
+  import { config } from '/@/utils/publicParamConfig';
+  import { useMessage } from '/@/hooks/web/useMessage';
+  import {
+    addStockList,
+    auditStockList,
+    getOneStockById,
+    StockEntity,
+    unAuditStockList,
+    updateStockList,
+  } from '/@/api/mainStock';
+  import { ValidateErrorEntity } from 'ant-design-vue/es/form/interface';
+  import { ExInput } from '/@/components/ExInput';
+  import { getPublicList } from '/@/api/public';
+  import { basicGridOptions } from '/@/components/AMoreSearch/data';
+  import { ControlSet, TableColum, Url } from '/@/api/apiLink';
+  import { VxeGridPropTypes } from 'vxe-table/types/all';
+  const { createMessage } = useMessage();
+  const AForm = Form;
+  const AFormItem = FormItem;
+  const ATextArea = Input.TextArea;
+  const router = useRouter();
+  const activeKey = ref<string>('1');
+  const formRef = ref();
+  const formRules = reactive({
+    name: [{ required: true, message: '请输入仓库名称' }],
+    number: [{ required: true, message: '请输入仓库编码' }],
+  });
+  //基础信息查询组件ref
+  const basicSearchRef: any = ref<any>(undefined);
+  const basicControl = ref<ControlSet[]>(); //下拉框
+  const basicTableCols = ref<VxeGridPropTypes.Columns[]>([]); //表头
+  const basicTableName = ref<string>(''); //表格数据
+  //初始化
+  const formData: StockEntity = {
+    id: undefined,
+    number: '',
+    name: '',
+  };
+  const formStateInit = reactive({
+    data: formData,
+  });
+  let formState = toRef(formStateInit, 'data');
+
+  //接受参数
+  let rowId = useRoute().query.row?.toString() || '';
+
+  //获取初始值
+  const getListById = async () => {
+    if (rowId) {
+      const res: any = await getOneStockById({ params: rowId });
+      formState.value = res;
+    }
+  };
+  getListById();
+  //保存事件
+  const onSubmit = async () => {
+    let data;
+    formRef.value
+      .validate()
+      .then(async () => {
+        if (!formState.value.id) {
+          data = await addStockList({ params: formState.value });
+        } else {
+          data = await updateStockList({ params: formState.value });
+        }
+        formState.value = Object.assign({}, formState.value, data);
+        createMessage.success('操作成功');
+      })
+      .catch((error: ValidateErrorEntity<FormData>) => {
+        createMessage.error('数据校检不通过，请检查!');
+        console.log(error);
+      });
+  };
+
+  //审核功能
+  const onExam = async () => {
+    formRef.value
+      .validate()
+      .then(async () => {
+        const type = await VXETable.modal.confirm('您确定要保存并审核当前数据吗?');
+        if (type === 'confirm') {
+          const data = await auditStockList({ params: formState.value });
+          formState.value = Object.assign({}, formState.value, data);
+          createMessage.success('操作成功');
+        }
+      })
+      .catch((error: ValidateErrorEntity<FormData>) => {
+        createMessage.error('数据校检不通过，请检查!');
+        console.log(error);
+      });
+  };
+  //反审核功能
+  const unExam = async () => {
+    const type = await VXETable.modal.confirm('您确定要反审核当前数据吗?');
+    if (type === 'confirm') {
+      const data = await unAuditStockList({ params: formState.value });
+      formState.value = Object.assign({}, formState.value, data);
+      createMessage.success('操作成功');
+    }
+  };
+  //点击清空图标清空事件
+  const onClear = (key: string[]) => {
+    key.forEach((e) => {
+      formState.value[e] = undefined;
+    });
+  };
+
+  let currDataParam: string[] = []; //约定数组下标0为数据ID，1为数据包
+  /**
+   * 基础资料弹窗
+   * @param dtoUrlConfig  获取基础资料查询链接属性
+   * @param tableName  指向的表名根据DTO链接可以查询到
+   * @param tableUrl  表格列表数据链接
+   * @param dataParam 当前选中的数据包
+   */
+  const onSearch: any = async (
+    dtoUrlConfig: string,
+    tableName: string,
+    tableUrl: string,
+    dataParam: string[],
+  ) => {
+    currDataParam = dataParam;
+    basicSearchRef.value.show();
+    const res = await getPublicList({ params: [] }, Url[dtoUrlConfig]);
+    basicControl.value = res as ControlSet[];
+    basicTableCols.value = TableColum[dtoUrlConfig];
+    basicTableName.value = tableName;
+    await basicSearchRef.value.init(tableUrl);
+  };
+  /**
+   * 基础资料弹框双击选择事件
+   * @param row
+   */
+  const basicClickEvent = async (row) => {
+    basicSearchRef.value.close();
+    formState.value[currDataParam[0]] = row.id;
+    formState.value[currDataParam[1]] = {};
+    formState.value[currDataParam[1]].id = row.id;
+    formState.value[currDataParam[1]].name = row.name;
+  };
+  //返回上一页
+  const back = () => {
+    router.go(-1);
+  };
+  //刚进入页面——加载完后，需要执行的方法
+  onMounted(() => {});
+</script>
+<style scoped lang="less">
+  .switchDiv {
+    width: 318px;
+  }
+  .item {
+    flex-flow: nowrap;
+  }
+  .switch {
+    flex-flow: nowrap;
+  }
+  .input {
+    width: 16rem;
+    height: 2rem;
+    margin: 0 60px 0 2px;
+  }
+  .textArea {
+    width: 16rem;
+    height: 2rem;
+    margin: 0 60px 0 2px;
+  }
+  .select {
+    width: 16rem;
+    margin: 0 60px 0 2px;
+  }
+  .content {
+    border: 1px solid #e5e7eb;
+    margin: 10px;
+    height: calc(100% - 80px);
+    background-color: #fff;
+    padding: 10px;
+  }
+  .button {
+    margin: 15px;
+  }
+</style>
---
+++ src/views/warehouse/purchase/return/index.vue
@@ -0,0 +1,240 @@
+<template>
+  <div style="height: 100%; padding: 15px">
+    <div style="background-color: #fff; height: 100%; padding: 0 6px">
+      <Search
+        :control="moreSearchData"
+        ref="searchRef"
+        tableName="BdStock"
+        searchNo="仓库编码："
+        searchName="仓库名称："
+        @getList="getList"
+        @resetEvent="resetTable"
+      />
+      <ExTable
+        :columns="stockColumns"
+        :gridOptions="GridOptions"
+        :importConfig="importConfig"
+        :tableData="tableData"
+        :totalData="totalData"
+        ref="tableRef"
+        @addTableEvent="addTableEvent"
+        @editTableEvent="editTableEvent"
+        @deleteRowEvent="deleteRowTableEvent"
+        @delBatchEvent="deleteBatchEvent"
+        @auditRowEvent="auditRowEvent"
+        @auditBatchEvent="auditBatchEvent"
+        @unAuditRowEvent="unAuditRowEvent"
+        @unAuditBatchEvent="unAuditBatchEvent"
+        @exportTable="exportTable"
+        @importModelEvent="importModelEvent"
+        @getList="getList"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts" name="basic-mainStock-index">
+  import { ExTable } from '/@/components/ExTable';
+  import { Search } from '/@/components/Search';
+  import { onActivated, onMounted, reactive, ref } from 'vue';
+  import { cloneDeep } from 'lodash-es';
+  import { gridOptions, stockColumns } from '/@/components/ExTable/data';
+  import { SearchParams } from '/@/api/apiLink';
+  import { OptTableHook } from '/@/api/utilHook';
+  import { useGo } from '/@/hooks/web/usePage';
+  import { PageEnum } from '/@/enums/pageEnum';
+  import {
+    getStockOption,
+    auditStockList,
+    auditStockListBatch,
+    delStockListBatch,
+    delStockListById,
+    exportStockList,
+    getStockTable,
+    importStockModel,
+    unAuditStockList,
+    unAuditStockListBatch,
+  } from '/@/api/mainStock';
+  import { useMessage } from '/@/hooks/web/useMessage';
+  const { createMessage } = useMessage();
+  const go = useGo();
+  const GridOptions = gridOptions;
+  const paneSize = ref<number>(16);
+  const installPaneSize = ref<number>(16);
+  //表格事件
+  const tableRef: any = ref<String | null>(null);
+  //表格数据
+  let tableData = ref<object[]>([]);
+  let totalData = ref<number>(0);
+  //查询组件
+  const searchRef: any = ref<String | null>(null);
+  //导入上传文件api
+  let importConfig = ref<string>('IMPORT_STOCK_LIST');
+  //分页信息
+  const pages = reactive({
+    currentPage: 1,
+    pageSize: 10,
+    total: 0,
+  });
+  let getParams: SearchParams[] = [];
+  //获取高级查询字段数据
+  const moreSearchData = ref();
+  getStockOption({ params: '' }).then((res) => {
+    moreSearchData.value = res;
+  });
+  //表格查询
+  const getList = async (currPage = 1, pageSize = pages.pageSize) => {
+    getParams = [];
+    if (searchRef.value.getSearchParams() && searchRef.value.getSearchParams().length > 0) {
+      getParams = getParams.concat(searchRef.value.getSearchParams());
+    }
+    //表格查询
+    const res: any = await getStockTable({
+      params: getParams,
+      orderByBean: {
+        descList: ['update_time'],
+      },
+      pageIndex: currPage,
+      pageRows: pageSize,
+    });
+    totalData.value = res.total;
+    pages.currentPage = currPage;
+    pages.pageSize = pageSize;
+    tableData.value = res.records;
+    searchRef.value.moreSearchClose();
+  };
+
+  //重置
+  const resetTable = () => {
+    searchRef.value.formState.wlNo = null;
+    searchRef.value.formState.wlName = null;
+    getList(1);
+  };
+  //添加
+  const addTableEvent = () => {
+    go({
+      path: PageEnum.MAIN_STOCK_DETAIL_AND_EDIT,
+    });
+  };
+  //编辑
+  const editTableEvent = (row) => {
+    go({
+      path: PageEnum.MAIN_STOCK_DETAIL_AND_EDIT,
+      query: {
+        row: row.id,
+      },
+    });
+  };
+  //删除表格单条数据
+  const deleteRowTableEvent = async (row) => {
+    await delStockListById({ params: row.id });
+    createMessage.success('删除成功');
+    await getList();
+  };
+  //批量删除表格
+  const deleteBatchEvent = async (rows: any[]) => {
+    const ids = rows.map((item) => {
+      return item.id;
+    });
+    const res = await delStockListBatch({ params: ids });
+    await tableRef.value.computeData(res);
+    await getList();
+  };
+  //审核单条
+  const auditRowEvent = async (row) => {
+    await auditStockList({
+      params: row,
+    });
+    createMessage.success('审核成功');
+    await getList();
+  };
+  //单条反审核
+  const unAuditRowEvent = async (row: any) => {
+    await unAuditStockList({
+      params: row,
+    });
+    createMessage.success('反审核成功');
+    await getList();
+  };
+  //批量审核事件
+  const auditBatchEvent = async (rows: any[]) => {
+    const ids = rows.map((item) => {
+      return item.id;
+    });
+    const res = await auditStockListBatch({
+      params: ids,
+    });
+    await tableRef.value.computeData(res);
+    await getList();
+  };
+
+  //批量反审核
+  const unAuditBatchEvent = async (rows: any[]) => {
+    const ids = rows.map((item) => {
+      return item.id;
+    });
+    const res = await unAuditStockListBatch({
+      params: ids,
+    });
+    await tableRef.value.computeData(res);
+    await getList();
+  };
+  //下载模板
+  const importModelEvent = async () => {
+    OptTableHook.importModel = (): Promise<any> => {
+      return new Promise((resolve, reject) => {
+        importStockModel({
+          params: '导入模板',
+        })
+          .then((res) => {
+            const data = { title: '仓库信息导入模板.xls', data: res };
+            resolve(data);
+          })
+          .catch((e) => {
+            reject(e);
+          });
+      });
+    };
+  };
+  //导出
+  const exportTable = async () => {
+    OptTableHook.exportExcel = (): Promise<any> => {
+      return new Promise((resolve, reject) => {
+        exportStockList({
+          params: {
+            list: getParams,
+            fileName: '仓库列表',
+          },
+          pageIndex: 1,
+          pageRows: pages.pageSize,
+        })
+          .then((res) => {
+            const data = { title: '仓库列表信息.xls', data: res };
+            resolve(data);
+          })
+          .catch((e) => {
+            reject(e);
+          });
+      });
+    };
+  };
+
+  onMounted(() => {
+    paneSize.value = cloneDeep(installPaneSize.value);
+    getList();
+  });
+  //被keep-alive 缓存的组件激活时调用
+  onActivated(() => {
+    getList();
+  });
+</script>
+
+<style scoped lang="less">
+  :deep(.ant-card-body) {
+    padding: inherit !important;
+  }
+  .tree-button {
+    margin: auto;
+    display: block;
+  }
+</style>
