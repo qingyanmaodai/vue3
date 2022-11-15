49e90caae872fcc2cc9d92f6670ed8ae72baf4d1
XX221104\bjr <bjr2022@163.com>
2022-11-15 09:31:20 +0800

    no message

--- src/views/warehouse/purchase/input/detail.vue
+++ src/views/warehouse/purchase/input/detail.vue
@@ -396,7 +396,7 @@
       })
       .catch((error: ValidateErrorEntity<FormData>) => {
         console.log(error);
-        if(error.errorFields) {
+        if (error.errorFields) {
           createMessage.error('数据校检不通过，请检查!');
         }
       });
@@ -431,7 +431,7 @@
       })
       .catch((error: ValidateErrorEntity<FormData>) => {
         console.log(error);
-        if(error.errorFields) {
+        if (error.errorFields) {
           createMessage.error('数据校检不通过，请检查!');
         }
       });
